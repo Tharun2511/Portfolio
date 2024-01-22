c52640cb3277feaee2c360108c23a0843744d74e
Tharun <gtharun2511@gmail.com>
2024-01-22 12:12:27 +0530

    z-index correction

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             >
                 <source src="/blackhole.webm" type="video/webm" />
             </video>
-            <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[3] px-10">
+            <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-[4] px-10">
                 <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
                     <Link
                         href="/"
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -14,7 +14,7 @@ const HeroContent = () => {
         <motion.div
             initial="hidden"
             animate="visible"
-            className="flex flex-row items-center justify-center px-20 mt-20 w-full z-[4] delay-700"
+            className="flex flex-row items-center justify-center px-20 mt-20 w-full z-[3] delay-700"
         >
             <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
                 <motion.div
