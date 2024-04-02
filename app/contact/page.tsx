cb0be448c1429511ef760fe9f84b489d41d58a41
Tharun <gtharun2511@gmail.com>
2024-04-02 16:37:04 +0530

    casual check

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { slideInFromTop, slideinFromBottom } from "@/utils/motion";
 import { MdOutlineCastConnected } from "react-icons/md";
+import { Comment } from "react-loader-spinner";
 import ContactContainer from "@/components/ContactContainer";
 
 const Contact = () => {
@@ -11,13 +12,22 @@ const Contact = () => {
         <motion.div
             initial="hidden"
             animate="visible"
-            className="w-full h-full flex flex-col z-[3] px-20 mt-20 gap-5"
+            className="w-full h-full flex flex-col z-[3] px-20 mt-20 gap-5 text-white"
         >
             <motion.div
-                className="text-white text-3xl mt-6 font-bold w-full h-1/4 flex gap-2"
+                className=" flex items-center text-3xl mt-6 font-bold w-full h-1/ gap-2"
                 variants={slideInFromTop}
             >
-                <MdOutlineCastConnected />
+                <Comment
+                    visible={true}
+                    height="40"
+                    width="40"
+                    ariaLabel="comment-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="comment-wrapper"
+                    color="#000"
+                    backgroundColor="#fff"
+                />
                 Let&apos;s{" "}
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
                     CONNECT
--- components/ContactContainer.tsx
+++ components/ContactContainer.tsx
@@ -1,65 +1,90 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { IoMdReturnRight } from "react-icons/io";
-import { toast, ToastContainer } from 'react-toastify';
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { InfinitySpin, TailSpin } from "react-loader-spinner";
 
 const ContactContainer = () => {
     const form = useRef<HTMLFormElement>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const notify = () => {
-        console.log("notify called");
-        toast("Default Notification !");
-  
-        toast.success("Success Notification !", {
-            position: "top-center"
-        });
-  
-        toast.error("Error Notification !", {
-          position: "top-left"
-        });
-  
-        toast.warn("Warning Notification !", {
-          position: "bottom-left"
-        });
-  
-        toast.info("Info Notification !", {
-          position: "bottom-center"
-        });
-  
-        toast("Custom Style Notification with css class!", {
-          position: "bottom-right",
-          className: 'foo-bar'
-        });
-      };
-
-  const sendEmail = () => {
-    
-    console.log(form);
-    emailjs
-      .sendForm('service_mhiuy05', 'template_4tu9arn', form.current!, {
-        publicKey: 'IK3oulfQ2ahujM78d',
-      })
-      .then(
-        () => {
-              console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+        emailjs
+            .sendForm("service_mhiuy05", "template_4tu9arn", form.current!, {
+                publicKey: "IK3oulfQ2ahujM78d",
+            })
+            .then(
+                () => {
+                    console.log("SUCCESS!");
+                    toast.success("Message Sent Successfully");
+                    form.current!.reset();
+                    setLoading(false);
+                },
+                (error) => {
+                    console.log("FAILED...", error.text);
+                    toast.error("Something went wrong");
+                    setLoading(false);
+                }
+            );
+    };
 
     return (
-        <div className="w-[800px] h-full border border-[#7e54ff61] bg-[#0300145a] shadow-lg shadow-[#2A0E61]/50 backdrop-blur-lg rounded-lg flex justify-center gap-6 mx-auto px-3 py-5">
-            <form className="flex flex-col gap-6" ref={form} onSubmit={sendEmail}>
+        <div className="w-[800px] h-full border border-[#7e54ff61] bg-[#0300145a] shadow-lg shadow-[#2A0E61]/50 backdrop-blur-lg rounded-lg flex justify-center gap-6 mx-auto px-3 py-10">
+            <form
+                className="flex flex-col gap-6"
+                ref={form}
+                onSubmit={(e) => sendEmail(e)}
+            >
                 <div className="flex gap-x-10 w-full">
-                    <input type="text" placeholder="Name" className="input" name='from_name' />
-                    <input type="text" placeholder="email" className="input" name='from_email'/>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        className="input"
+                        name="from_name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="input"
+                        name="from_email"
+                        required
+                    />
                 </div>
-                <input type="text" placeholder="subject" className="input" name='subject'/>
-                <textarea placeholder="message" className="textarea" name='message'></textarea>
-                <button className="btn text-white text-2xl font-sans bg-purple-800 h-[50px] rounded-2xl hover:bg-purple-400 flex gap-6 justify-center items-center transition-all duration-300" type='submit' onClick={notify}>Let&apos;s Talk<IoMdReturnRight className="text-3xl"/></button>
+                <input
+                    type="text"
+                    placeholder="Subject"
+                    className="input"
+                    name="subject"
+                    required
+                />
+                <textarea
+                    placeholder="Message"
+                    className="textarea"
+                    name="message"
+                    rows={10}
+                    required
+                ></textarea>
+                <button
+                    className="btn text-white text-2xl font-sans bg-purple-800 h-[50px] rounded-2xl hover:bg-purple-400 flex gap-6 justify-center items-center transition-all duration-300"
+                    type="submit"
+                >
+                    {!loading && <div className="flex items-center gap-6">
+                        Let&apos;s Talk
+                        <IoMdReturnRight className="text-3xl" />
+                    </div>}
+                    <TailSpin
+                        visible={loading}
+                        height="40"
+                        width="40"
+                        color="#fff"
+                        ariaLabel="tail-spin-loading"
+                        radius="1"
+                    />
+                </button>
             </form>
         </div>
     );
